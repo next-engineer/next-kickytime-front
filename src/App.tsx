23c9874a7136638c4b855408da7fba0029a00c74
Mminzy22 <aw7022@gmail.com>
2025-08-12 01:29:08 +0900

    ✨ feat: 환경변수 검증 추가 및 라우터 구조 개선

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,6 @@
-import { RouterProvider } from 'react-router-dom';
-import { ThemeProvider, CssBaseline } from '@mui/material';
-import { theme } from './styles/theme';
-import { router } from './routes';
+import { RouterProvider } from "react-router-dom"
+import { router } from "./routes"
 
 export default function App() {
-  return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <RouterProvider router={router} />
-    </ThemeProvider>
-  );
+  return <RouterProvider router={router} />
 }
