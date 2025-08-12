4ee7194f2ac1950573244a844f45bf08b8e1243c
imjwoo <jeongu8807@daum.com>
2025-08-12 11:00:29 +0900

    ✨ feat(auth): cognito config 설정

---
+++ auth/config.ts
@@ -0,0 +1,6 @@
+export const COGNITO = {
+  hostedUiDomain: 'https://ap-northeast-2xi18xw5er.auth.ap-northeast-2.amazoncognito.com',
+  clientId: '6l5ovdmof4vc5r2socegk41gt4',
+  redirectUri: 'http://localhost:5173/',
+  scopes: ['openid', 'email'],
+};
