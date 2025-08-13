ee95f415ba4e098d273d554095c4a7f088a40afa
H0H <dihexyldecyl@gmail.com>
2025-08-13 19:55:40 +0900

    ✨ feat(auth): 라우트 가드 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,79 @@
-import { RouterProvider } from 'react-router-dom';
-import { ThemeProvider, CssBaseline } from '@mui/material';
-import { theme } from './styles/theme';
-import { router } from './routes';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import { ProtectedRoute, AdminRoute, PublicOnlyRoute } from './components/RouteGuards';
+
+// common page
+import HomePage from './pages/HomePage';
+import NotFoundPage from './pages/NotFoundPage';
+import MatchesPage from './pages/MatchesPage';
+
+// public only page
+import SignupPage from './pages/SignupPage';
+import LoginPage from './pages/LoginPage';
+
+// protected page
+import ProfilePage from './pages/ProfilePage';
+import MyMatchesPage from './pages/MyMatchesPage';
+
+// admin page
+import CreateMatchPage from './pages/CreateMatchPage';
 
 export default function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <RouterProvider router={router} />
-    </ThemeProvider>
+    <BrowserRouter>
+      <Routes>
+        {/* 누구나 접근 가능한 페이지 */}
+        <Route path="/" element={<HomePage />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/notfound" element={<NotFoundPage />} />
+        <Route path="/matches" element={<MatchesPage />} />
+
+        {/* 로그인하지 않은 사용자만 접근 가능 */}
+        <Route
+          path="/login"
+          element={
+            <PublicOnlyRoute>
+              <LoginPage />
+            </PublicOnlyRoute>
+          }
+        />
+        <Route
+          path="/signup"
+          element={
+            <PublicOnlyRoute>
+              <SignupPage />
+            </PublicOnlyRoute>
+          }
+        />
+
+        {/* 로그인한 사용자만 접근 가능 */}
+        <Route
+          path="/profile"
+          element={
+            <ProtectedRoute>
+              <ProfilePage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/mymatches"
+          element={
+            <ProtectedRoute>
+              <MyMatchesPage />
+            </ProtectedRoute>
+          }
+        />
+
+        {/* 관리자만 접근 가능 */}
+        <Route
+          path="/createMatch"
+          element={
+            <AdminRoute>
+              <CreateMatchPage />
+            </AdminRoute>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
